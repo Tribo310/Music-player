e5a8884129e26fdf24afcecc5b1a393be15953e7
Tribo310 <monhochir0131@gmail.com>
2024-06-07 01:07:42 +0800

    first commit

---
+++ data.js
@@ -0,0 +1,32 @@
+let songs = [
+    {
+        name: 'Radiohead ',
+        path: 'musics/Song1.mp3',
+        artist: 'Creep',
+        cover: 'images/images/img1.png'
+    },
+    {
+        name: 'Azure',
+        path: 'musics/song2.mp3',
+        artist: 'Gemlest',
+        cover: 'images/images/cover\ 2.png'
+    },
+    {
+        name: 'Бид хоёр',
+        path: 'musics/song3.mp3',
+        artist: 'Rokit Bay',
+        cover: 'images/images/img2.jpg'
+    },
+    {
+        name: 'Нэг өдрийн хаан',
+        path: 'musics/song4.mp3',
+        artist: 'Rokit Bay',
+        cover: 'images/images/img4.jpg'
+    },
+    {
+        name: 'Чи миний',
+        path: 'musics/song5.mp3',
+        artist: '"Guys" хамтлаг',
+        cover: 'images/images/img5.jpg'
+    },
+]
